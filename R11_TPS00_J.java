c42d5ce41b92981eab7db2e60b2a33b3ef10da1c
Munchootsorn Wangsriviroj <munchootsorn@gmail.com>
2025-03-02 20:48:55 -0500

    Create R11_TPS00_J.java

---
+++ R11_TPS00_J.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class R11_TPS00_J {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        executor.submit(() -> {
+            // Time-consuming task
+        });
+    }
+}
