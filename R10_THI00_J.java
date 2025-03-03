edf7b01a98ca1b49adec70a8c70b7c4fc90289d5
Munchootsorn Wangsriviroj <munchootsorn@gmail.com>
2025-03-02 20:46:01 -0500

    Create R10_THI00_J.java

---
+++ R10_THI00_J.java
@@ -0,0 +1,10 @@
+public class R10_THI00_J {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                System.out.println("Running thread");
+            }
+        });
+        thread.run(); // Violation
+    }
+}
