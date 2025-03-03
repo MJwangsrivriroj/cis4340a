09cc174d36128837f81b61648952b19c3d0d7a71
Munchootsorn Wangsriviroj <munchootsorn@gmail.com>
2025-03-02 20:43:07 -0500

    Fix the Vulnerability for R09_LCK00_J.java

--- R09_LCK00_J.java
+++ R09_LCK00_J.java
@@ -1,6 +1,9 @@
 public class R09_LCK00_J {
     private int count = 0;
-    public synchronized void increment() {
-        count++;
+    private final Object lock = new Object();
+    public void increment() {
+        synchronized (lock) {
+            count++;
+        }
     }
 }
